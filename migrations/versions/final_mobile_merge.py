3f44afe19b2c44ba2d54d6f7d2788974dead5a63
guylikesweet <francisakubueze50@gmail.com>
2026-09-24 14:39:29 +0100

    Update down_revision to include additional revision ID

--- migrations/versions/final_mobile_merge.py
+++ migrations/versions/final_mobile_merge.py
@@ -1,17 +1,15 @@
 """merge the existing schema branches for the consolidated production package
 
 Revision ID: f7a1c9e4b210
-Revises: 24ba0daf2a17, e71c8b4f2d10
+Revises: 24ba0daf2a17, e71c8b4f2d10, 7f0f6f0c8a21
 """
 revision = "f7a1c9e4b210"
-down_revision = ("24ba0daf2a17", "e71c8b4f2d10")
+down_revision = ("24ba0daf2a17", "e71c8b4f2d10", "7f0f6f0c8a21")
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
     pass
-
 
 def downgrade():
     pass
