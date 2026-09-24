acde20068a3375a27248c9970c9abac2f20c71e4
guylikesweet <francisakubueze50@gmail.com>
2026-09-24 15:11:30 +0100

    Fix down_revision format in migration file

--- migrations/versions/final_mobile_merge.py
+++ migrations/versions/final_mobile_merge.py
@@ -4,7 +4,7 @@ Revision ID: f7a1c9e4b210
 Revises: 24ba0daf2a17, e71c8b4f2d10, 7f0f6f0c8a21
 """
 revision = "f7a1c9e4b210"
-down_revision = ("24ba0daf2a17", "e71c8b4f2d10", "7f0f6f0c8a21")
+down_revision = ('24ba0daf2a17', 'e71c8b4f2d10')
 branch_labels = None
 depends_on = None
 
